3dc2486d3bbb03bee34a2178b5d715f9f50cabfa
Steve Molitor <stevemolitor@gmail.com>
2010-04-22 15:06:10 -0500

    adding relationships_spec.rb

---
+++ spec/relations_spec.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Positional relations" do
+  DATA = <<END
+ORD3456
+ORD_Lab
+ORD_Lcd
+END
+
+  class Order < Pancakes::PositionalPancake
+    field :data, :position => 3..6
+     has_many :order_lines, :key => "ORD_L"
+  end
+
+  class OrderLine < Pancakes::PositionalPancake
+    field :data, :position => 5..6
+  end
+
+  before :each do
+    @order = Order.load(DATA)
+  end
+
+  it "should handle one to many relationships" do
+    @order.data.should == '3456'
+    @order.order_lines.size.should == 2
+    @order.order_lines.first.data.should == 'ab'
+    @order.order_lines.last.data.should == 'cd'
+  end
+end
